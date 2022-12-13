75f011afb980c7951ef258143b83b911b5872e78
Nickunj Chopra <nickunjchopra@gmail.com>
2022-12-13 19:13:36 +0530

    Switching from gTTS to official Google Cloud TTS.

--- audio.py
+++ audio.py
@@ -5,12 +5,12 @@ from better_profanity import profanity
 def writeAudio(text, objID, objType):
   filename = 'audio/{}_{}.mp3'.format(objType, objID)
 
-  tts = gTTS(profanity.censor(text, '-'), lang='en', tld='com.au', slow=False)
+  tts = gTTS(profanity.censor(text, '-'), lang='en', tld='ie', slow=False)
   with open(filename, 'wb') as f:
     tts.write_to_fp(f)
   
-  snippet = AudioSegment.from_file(filename, format='mp3')
-  spedup = snippet.speedup(playback_speed=1.2)
-  spedup.export(filename)
+  # snippet = AudioSegment.from_file(filename, format='mp3')
+  # spedup = snippet.speedup(playback_speed=1.1)
+  # spedup.export(filename)
 
   return filename
