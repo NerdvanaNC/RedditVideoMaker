677da193637679e1380d8c68647bea5bb0fd1c3e
Nickunj Chopra <nickunjchopra@gmail.com>
2023-03-13 17:20:56 +0530

    Reddit changed around their landing interface so had to wrestle with it to take good screenshots again. :)

--- screenshots.py
+++ screenshots.py
@@ -7,22 +7,28 @@ def screenshot(link, objID, objType):
   with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
-    device = p.devices['iPhone 12']
+    device = p.devices['iPhone X']
     page = browser.new_page(**device, color_scheme='light')
     link = link if 'https://www.reddit.com/' in link else 'https://www.reddit.com{}'.format(link)
     page.goto(link)
     page.wait_for_load_state('networkidle')
 
     locatorTag = 'shreddit-post' if objType == 'post' else 'shreddit-comment-tree'
-    continueButtonTag = 'button#secondary-button.continue'
+    openInAppBar = 'shreddit-async-loader[bundlename="bottom_bar_xpromo"]'
+    iPhonePrompt = 'shreddit-async-loader[bundlename="educational_deeplink_prompt_for_ios"]'
 
     if page.locator(locatorTag).first.is_visible():
-      page.locator(continueButtonTag).click()
+      if page.locator(openInAppBar).is_visible():
+        page.evaluate('''(openInAppBar) => document.querySelector(openInAppBar).remove()''', openInAppBar)
+      elif page.locator(iPhonePrompt).is_visible():
+        page.evaluate('''(iPhonePrompt) => document.querySelector(iPhonePrompt).remove()''', iPhonePrompt)
       if objType != 'post':
         page.evaluate('''(commentChildren) => document.querySelector(commentChildren).remove()''', '[slot="children"]')
         page.evaluate('''(commentTools) => document.querySelector(commentTools).remove()''', 'shreddit-comments-page-tools')
+        if page.locator('button:has-text("Continue")').is_visible():
+          page.locator('button:has-text("Continue")').click()
+      page.locator(locatorTag).first.click()
       page.locator(locatorTag).first.screenshot(path=filename)
-    
     browser.close()
 
   return filename
